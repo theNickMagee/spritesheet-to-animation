60ec21ce2d972f0e5a37c98e7c7571ff67fcb716
theNickMagee <59543756+theNickMagee@users.noreply.github.com>
2020-01-05 18:00:53 -0600

    Create sprite.js

---
+++ sprite.js
@@ -0,0 +1,21 @@
+class Sprite {
+  constructor(animation, x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.animation = animation;
+    this.w = this.animation[0].width;
+    this.len = this.animation.length;
+    this.speed = speed;
+    this.index = 0;
+  }
+
+  show() {
+    let index = floor(this.index) % this.len;
+    image(this.animation[index], this.x, this.y, width, height);
+  }
+
+  animate() {
+    this.index += this.speed;
+    console.log(this.index);
+  }
+}
